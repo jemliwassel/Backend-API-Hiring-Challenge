6c3a7f05372594cfb089d937e32612bb1a84b722
jemliwassel <wassel.jemli@etudiant-enit.utm.tn>
2022-12-30 20:42:53 +0100

    feat: ajout d'une nouvelle fonction get_dataset_info

--- main.py
+++ main.py
@@ -21,6 +21,35 @@ def get_dataset(id: int):
         raise HTTPException(status_code=404, detail="Dataset not found")
     return datasets[id]
 
+@app.get("/datasets/")
+async def list_datasets():
+    data1 = {}
+    for id in data : 
+        df = data[id]
+        res = df.to_json(orient="records")
+        parsed = json.loads(res)
+        data1[id] = parsed   #Dictionary of datasets 
+    return data1
+       
+@app.post("/datasets/")
+async def create_dataset(csv_file: UploadFile = File(...)):
+    try: 
+        df = pd.read_csv(csv_file.file)
+        id = len(data)  # use the number of datasets as the id for the new dataset
+        data[id] = df
+        datasets[id] = csv_file
+    except Exception as e:
+        raise HTTPException(status_code=400, detail= "Unable to read CSV file")
+    return {"id": id}
+
+@app.get("/datasets/{id}/")
+def get_dataset_info(id: int, dataset = Depends(get_dataset)):
+        return {"filename": dataset, "size": len(data[id])}
+    
+
+
+
+
 
 
 
